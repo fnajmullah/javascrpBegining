02d59ef628fb323b6cfaf4ba20135f1eac507bf9
ferdun <najeeb@2u.chat>
2022-06-07 11:39:22 +0300

    testing javascript

---
+++ newfolder/console/app.js
@@ -0,0 +1,84 @@
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+// document.getElementById("element").style.color="grey";
+
+// this is comment and used for big code program
+// console.log(["Ahmad","Mahmood"]);
+
+
+
+// console.warn("this is warning");
+// console.error("this is error");
+// console.clear();
+
+// var a=20;
+// let b=30;
+// const c=34;
+// console.log(a,b,c);
+// console.log(typeof a);
+
+// var can be defined many times and replace the value
+// let can be defined only once and values can be changed
+// const can not be changed if data type is primitive and if reference than can be changed. becuase it is referenceing to the object
+
+// let a;
+
+// a=3243;
+
+// console.log(typeof String(a));
+
+let value;
+value=String(234234);
+console.log(value);
+console.log(typeof value);
+
+console.log("string value number to number typecasting");
+value=Number("23423");
+console.log(value);
+console.log(typeof value);
+
+console.log("null to number typecasting");
+value=Number(null);
+console.log(value);
+console.log(typeof value);
+
+console.log("undefined to number typecasting");
+value=Number(undefined);
+console.log(value);
+console.log(typeof value);
+
+console.log("string to number typecasting");
+value=Number("hellow world");
+console.log(value);
+console.log(typeof value);
+
+console.log("function to number typecasting");
+value=Number(function(){
+    console.log()
+});
+console.log(value);
+console.log(typeof value);
+
+console.log("array to number typecasting");
+value=Number([1,3,5,6]);
+console.log(value);
+console.log(typeof value);
+
+console.log("check assigned string and number and then check the data type it is string");
+const a="helloworld"+34;
+console.log(a);
+console.log(typeof a);
+
+let va;
+console.log(va);
+console.log(typeof va);
